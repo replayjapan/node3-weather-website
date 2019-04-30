6ad0ce6be48f67de7ad148553820490bca401341
rePlay <CraigMBP@craigs-mbp.lan>
2019-04-30 20:53:54 +0900

    Set up app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ messageTwo.textContent = ''
     const location = search.value
 
 
-    fetch('http://localhost:3000/weather?address='+location).then((response => {
+    fetch('/weather?address='+location).then((response => {
     response.json().then((data)=>{
         if (data.error) {
             messageOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const forecast = require('./utils/forecast')
 // console.log(path.join(__dirname,'../public'))
 
 const app = express ()
+const port = process.env.PORT || 3000
 
 // Define for Express Config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -129,6 +130,6 @@ app.get('*',(req,res) => {
 
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up')
 })
